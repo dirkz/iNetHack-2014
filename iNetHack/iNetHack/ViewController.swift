42dda689b67ba167f63a2d5498cde8959d429b5e
Dirk Zimmermann <me@dirkz.com>
2014-10-18 15:10:01 +0200

    SingleView Xcode project

---
+++ iNetHack/iNetHack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iNetHack
+//
+//  Created by dirk on 18/10/14.
+//  Copyright (c) 2014 Dirk Zimmermann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
